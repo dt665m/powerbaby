efb4c793de5a05fcd9d0712fe90e65d8d048795f
Denis Tsai <denistsai@aetheras.io>
2023-04-09 05:38:41 +0800

    add teaser website

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/yoshiegg.png" className="App-logo" alt="logo" />
+      <p>Hatching Soon</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
